bf5e7a97b29a2a6877c0a40f5d46d1a913f55926
Kanbaru <lihaochen910@hotmail.com>
2018-05-01 14:51:52 +0800

    添加代码
    Signed-off-by: Kanbaru <lihaochen910@hotmail.com>

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	player = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (PressurePlate != nullptr)
+	{
+		if (PressurePlate->IsOverlappingActor(player) && this->doorState == DoorState::Close)
+		{
+			OpenDoor();
+			doorState = DoorState::Open;
+		}
+		if (!PressurePlate->IsOverlappingActor(player) && this->doorState == DoorState::Open)
+		{
+			CloseDoor();
+			doorState = DoorState::Close;
+		}
+	}
+}
+
+void UOpenDoor::OpenDoor()
+{
+	AActor* Owner = GetOwner();
+
+	FRotator NewRotation = FRotator(0, OpenAngle, 0);
+
+	Owner->SetActorRotation(NewRotation);
+}
+
+void UOpenDoor::CloseDoor()
+{
+	AActor* Owner = GetOwner();
+
+	FRotator NewRotation = FRotator(0, -180, 0);
+
+	Owner->SetActorRotation(NewRotation);
+}
---
+++ Source/BuildingEscape/OpenDoor.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "OpenDoor.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+	enum class DoorState {
+		Open,
+		Close
+	};
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void OpenDoor();
+	void CloseDoor();
+		
+private:
+	UPROPERTY(EditAnywhere)
+		float OpenAngle = 90.0f;
+
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate;
+
+	UPROPERTY(VisibleAnywhere)
+		AActor* player;
+
+
+	DoorState doorState = DoorState::Close;
+};
